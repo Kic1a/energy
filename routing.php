589c7b3379c67acf32463d606484accbe742806e
User <User@Piotrek>
2024-08-23 23:38:25 +0200

    v3 zaplanowany routing

--- routing.php
+++ routing.php
@@ -7,6 +7,10 @@ App::getRouter()->setDefaultRoute('login'); #default action
 App::getRouter()->setLoginRoute('login'); #action to forward if no permissions
 
 Utils::addRoute('login', 'loginCtrl');
-Utils::addRoute('deviceList',     'deviceCtrl',     ['user','admin']);
+Utils::addRoute('deviceList',   'deviceCtrl',     ['user','admin']);
+Utils::addRoute('deviceNew',    'deviceEditCtrl',     ['user','admin']);
+Utils::addRoute('deviceEdit',   'deviceEditCtrl',     ['user','admin']);
+Utils::addRoute('deviceSave',   'deviceCtrl',     ['user','admin']);
+Utils::addRoute('deviceDelete', 'deviceCtrl',     ['admin']);
 
 
